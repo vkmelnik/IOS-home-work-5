3c3bf4cb7b24b3332fe3fe3d9f01f5037528a7d6
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2021-11-11 13:58:03 +0300

    Initial commit, storyboard is deleted

---
+++ vkmelnikPW5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vkmelnikPW5
+//
+//  Created by Vsevolod Melnik on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
