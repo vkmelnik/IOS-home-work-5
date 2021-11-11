1024309823508c800e8d76311e042c187bcd05c8
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2021-11-11 15:17:44 +0300

    ArticleManager loads articles from API

--- vkmelnikPW5/Models/ArticleManager.swift
+++ vkmelnikPW5/Models/ArticleManager.swift
@@ -20,4 +20,26 @@ class ArticleManager {
             observer?.updateArticles()
         }
     }
+    
+    private func getURL(_ rubric: Int, _ pageIndex: Int) -> URL? {
+        URL(string: "https://news.myseldon.com/api/Section?rubricId=\(rubric)&pageSize=8&pageIndex=\(pageIndex)"
+        )
+    }
+    
+    // MARK: - Fetch news
+    private func fetchNews() {
+        guard let url = getURL(4, 1) else { return }
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            if let data = data {
+                var articlePage = try?
+                    JSONDecoder().decode(ArticlePage.self, from: data)
+                articlePage?.passTheRequestId()
+                self?.Articles = (articlePage?.news)!
+            }
+        }.resume()
+    }
 }
