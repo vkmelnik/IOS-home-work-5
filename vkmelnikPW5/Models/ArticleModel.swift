9ca55a0d57f4b22b7f6edc5b6044aaac729362b7
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2021-11-11 14:22:24 +0300

    Struct for ArticleModel is created

---
+++ vkmelnikPW5/Models/ArticleModel.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleModel.swift
+//  vkmelnikPW5
+//
+//  Created by Vsevolod Melnik on 11.11.2021.
+//
+
+import Foundation
+
+// Struct that contains information about article.
+struct ArticleModel {
+    var title: String
+    var description: String
+    var imageLink: URL
+    var articleLink: URL
+}
